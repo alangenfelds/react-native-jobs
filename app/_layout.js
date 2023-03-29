521d5d936f42390548a1154d07d454dd1089c6e5
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-03-29 12:16:41 +0300

    Welcome screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,29 @@
-import {Stack} from 'expo-router'
+import { useCallback } from "react";
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
-import React from 'react'
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+import React from "react";
 
 const Layout = () => {
-  return (
-   <Stack />
-  )
-}
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
 
-export default Layout
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView} />;
+};
+
+export default Layout;
